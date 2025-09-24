11af196ca24d6a40e836a8e95b8610140832d03f
thisalsalpura <salpurathisal@gmail.com>
2025-09-24 19:46:08 +0530

    Update 2.6

--- app/src/screens/AvatarAdding.tsx
+++ app/src/screens/AvatarAdding.tsx
@@ -1,12 +1,35 @@
-import { KeyboardAvoidingView, Platform, Text, View } from "react-native";
+import { Image, KeyboardAvoidingView, Platform, Pressable, Text, View } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CircleShape from "../components/CircleShape";
 import { StatusBar } from "expo-status-bar";
 import { SvgUri } from "react-native-svg";
 import * as ImagePicker from 'expo-image-picker';
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faCamera, faUser } from "@fortawesome/free-regular-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { useTheme } from "../theme/ThemeProvider";
 
 export default function AvatarAdding() {
+
+    const { applied } = useTheme();
+
+    const [image, setImage] = useState<string | null>(null);
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ["images"],
+            allowsEditing: true,
+            aspect: [3, 3],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+        }
+    }
+
     return (
         <SafeAreaView className="flex-1 bg-sand-400" edges={["top", "bottom"]}>
             <KeyboardAvoidingView className="flex-1" behavior={Platform.OS === "android" ? "padding" : "height"}>
@@ -26,12 +49,34 @@ export default function AvatarAdding() {
                                 <Text className="text-black text-lg font-EncodeSansCondensedMedium tracking-widest">Step 4</Text>
                             </View>
                         </View>
+
+                        <View className="mt-10 h-auto w-full flex flex-row justify-center items-center">
+                            <Pressable className="relative h-44 w-44 bg-[#1C1C21] dark:bg-white justify-center items-center border-2 border-gray-400 border-dashed rounded-full p-1">
+                                {image && (
+                                    <Image source={{ uri: image }} className="h-full w-full rounded-full" />
+                                )}
+                                <View className="absolute bottom-0 right-0 translate-x-1/2 translate-y-1/2 h-12 w-12 bg-[#1C1C21] dark:bg-white flex justify-center items-center border-2 border-gray-400 border-dashed rounded-full">
+                                    <FontAwesomeIcon icon={faUser as IconProp} color={applied === "dark" ? "#000000" : "#FFFFFF"} size={18} />
+                                </View>
+                            </Pressable>
+                        </View>
+
+                        <View className="mt-10 h-auto w-full flex justify-center items-center">
+                            <Pressable className="h-auto w-full bg-[#1C1C21] dark:bg-white flex flex-col justify-center items-center border-2 border-gray-400 border-dashed rounded-[10px] p-5 gap-4">
+                                <FontAwesomeIcon icon={faCamera as IconProp} color={applied === "dark" ? "#000000" : "#FFFFFF"} size={34} />
+                                <Text className="text-white dark:text-black text-lg font-EncodeSansCondensedMedium tracking-widest">Choose a Profile Picture</Text>
+                            </Pressable>
+                        </View>
+
+                        <View className="mt-10 h-auto w-full flex justify-center items-center">
+                            <Text className="text-black dark:text-white text-lg font-EncodeSansCondensedMedium tracking-widest">Or you can choose a Avatar!</Text>
+                        </View>
                     </View>
                 </KeyboardAwareScrollView>
             </KeyboardAvoidingView>
 
-            <CircleShape height={200} width={200} fillColor="#D5BDAF" borderRadius={999} bottomValue={-30} rightValue={-95} />
-            <CircleShape height={150} width={150} fillColor="#D5BDAF" borderRadius={999} bottomValue={-40} rightValue={45} />
+            {/* <CircleShape height={200} width={200} fillColor="#D5BDAF" borderRadius={999} bottomValue={-30} rightValue={-95} />
+            <CircleShape height={150} width={150} fillColor="#D5BDAF" borderRadius={999} bottomValue={-40} rightValue={45} /> */}
 
             <StatusBar hidden={true} />
         </SafeAreaView>
